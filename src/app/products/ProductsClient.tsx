42950c91958f69d80a7d564badd97b29d38e0660
mark <mark.mekhayl@gmail.com>
2025-11-17 17:27:13 +1100

    ts

--- src/app/products/ProductsClient.tsx
+++ src/app/products/ProductsClient.tsx
@@ -207,7 +207,7 @@ export default function ProductsClient({ products, categoryNames }: ProductsClie
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     {categoryProducts.map((product) => (
-                      <ProductCard key={product.id} product={product} loadingPrices={loadingPrices} dataTimestamp={dataTimestamp} />
+                      <ProductCard key={product.id} product={product} loadingPrices={loadingPrices} dataTimestamp={dataTimestamp as Date} />
                     ))}
                 </div>
             </div>
